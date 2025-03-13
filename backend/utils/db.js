acc4954b215d1f0a1104f555cee82ac3502687cc
mizansutar <sutarmizan6@gmail.com>
2025-03-13 22:24:31 +0530

    the user model is created in models / user_models file and there are

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+
+//" connection the mongodb with the server for the data saving "
+
 const connectDB=async () => {
     try {
        await mongoose.connect(process.env.mongoURL);
