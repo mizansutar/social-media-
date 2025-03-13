4a86f5251f1b07b0b288145cac9a4b5329daf768
mizansutar <sutarmizan6@gmail.com>
2025-03-13 23:26:10 +0530

    the is created and in this login and logout controller is created

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs"
 import { User } from "../models/user_models.js"
-
+import jwt from "jsonwebtoken"
 export const register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -33,3 +33,62 @@ export const register = async (req, res) => {
     }
 }
 
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(401).json({
+                message: "something is missing please check..!",
+                success: false,
+            });
+        }
+
+        let user = await User.findOne({ email })
+        if (!user) {
+            return res.status(401).json({
+                message: "incorrect email or password",
+                success: false,
+            })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (isPasswordMatch) {
+            return res.status(401).json({
+                message: "incorrect email or password",
+                success: false,
+            })
+        }
+        user = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic,
+            bio: user.bio,
+            follower: user.follower,
+            following: user.following,
+            posts: user.posts
+        }
+
+        const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' })
+        return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 1000 }).json({
+            message: `welcome back ${username}`,
+            success: true,
+            user
+        })
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        return res.cookie("token" | "", { maxAge: 0 }).json({
+            message: " logout succesfully",
+            success: true,
+
+        })
+    } catch (error) {
+        console.log(error);
+    }
+} 
